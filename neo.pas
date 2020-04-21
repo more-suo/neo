7e3a5b3773a70fbafe121ce03577b5bfc59b55f8
arcturus5340 <arcturus5340@gmail.com>
2020-04-21 23:06:42 +0200

    Optimized .operator/()

--- neo.pas
+++ neo.pas
@@ -451,9 +451,8 @@ implementation
       if number = 0 then
         raise new System.ArithmeticException('ZeroDivisionError');
       var tmp_result := new single[self_ndarray.length];
-      var item_gen_a := self_ndarray.__get_item_generator();
-      for var index := 0 to self_ndarray.length-1 do
-        tmp_result[index] := item_gen_a() / number; 
+      for var i := 0 to self_ndarray.length-1 do
+        tmp_result[i] := self_ndarray.value[i] / number; 
       Result := new ndarray(tmp_result, self_ndarray.shape);
     end;
 
@@ -461,9 +460,8 @@ implementation
     class function ndarray.operator/(number: single; self_ndarray: ndarray): ndarray;
     begin
       var tmp_result := new single[self_ndarray.length];
-      var item_gen_a := self_ndarray.__get_item_generator();
-      for var index := 0 to self_ndarray.length-1 do
-        tmp_result[index] := number / item_gen_a(); 
+      for var i := 0 to self_ndarray.length-1 do
+        tmp_result[i] := number / self_ndarray.value[i]; 
       Result := new ndarray(tmp_result, self_ndarray.shape);
     end;
 
@@ -473,10 +471,8 @@ implementation
       if not areEqual(self_ndarray.shape, other_ndarray.shape) and (self_ndarray.rank <> 1) and (other_ndarray.rank <> 1) then
         raise new System.ArithmeticException('Wrong array sizes');
       var tmp_result := new single[self_ndarray.length];
-      var item_gen_a := self_ndarray.__get_item_generator();
-      var item_gen_b := other_ndarray.__get_item_generator();
-      for var index := 0 to self_ndarray.length-1 do
-        tmp_result[index] := item_gen_a() / item_gen_b(); 
+      for var i := 0 to self_ndarray.length-1 do
+        tmp_result[i] := self_ndarray.value[i] / other_ndarray.value[i]; 
       Result := new ndarray(tmp_result, self_ndarray.shape);  
     end;
 
